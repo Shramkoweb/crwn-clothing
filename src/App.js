155a351b6b2965785d59a40549e18e497c5d1e32
serhii.shramko <shramko.web@yahoo.com>
2020-02-23 15:31:00 +0200

    chore: update App.js & add base styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from "./pages/homepage/homepage.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
+import './App.scss';
+
+import HomePage from './pages/homepage/homepage.component';
+import Shop from './pages/shop/shop.component';
 
 const App = () => (
   <div>
     <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route exact path='/hats' component={HatsPage}/>
+      <Route exact path='/shop' component={Shop}/>
     </Switch>
   </div>
 );
