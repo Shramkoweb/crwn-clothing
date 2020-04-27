b7c52f58731bbfe8455b7fdd0db543deb948f109
serhii.shramko <shramko.web@yahoo.com>
2020-04-27 23:03:49 +0300

    feat: implement add item to cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,18 +5,21 @@ import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, cartItemsAmount }) => {
   return (
     <button type='button' className='cart-icon' onClick={toggleCartHidden}>
       <ShoppingIcon className='shopping-icon'/>
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartItemsAmount}</span>
     </button>
   );
 };
 
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItemsAmount: cartItems.length
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
