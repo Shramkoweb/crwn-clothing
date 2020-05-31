dfce31418450892eff54bdbc4896c5b1c209f538
serhii.shramko <shramko.web@yahoo.com>
2020-05-31 23:33:08 +0300

    refactor: change handleSubmit & handleChange methods

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -15,9 +15,12 @@ class SignUp extends React.PureComponent {
       password: '',
       confirmPassword: '',
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleSubmit = async (event) => {
+  async handleSubmit(event) {
     event.preventDefault();
 
     const {displayName, email, password, confirmPassword} = this.state;
@@ -29,8 +32,8 @@ class SignUp extends React.PureComponent {
 
     try {
       const {user} = await auth.createUserWithEmailAndPassword(
-          email,
-          password,
+        email,
+        password,
       );
 
       await createUserProfileDocument(user, {displayName});
@@ -42,64 +45,64 @@ class SignUp extends React.PureComponent {
         confirmPassword: '',
       });
     } catch (error) {
-      console.error(error);
+      throw new Error(error);
     }
-  };
+  }
 
-  handleChange = ({target}) => {
+  handleChange({target}) {
     const {name, value} = target;
 
     this.setState({[name]: value});
-  };
+  }
 
   render() {
     const {displayName, email, password, confirmPassword} = this.state;
     return (
-        <SignUpContainer>
-          <SignUpTitle>I do not have a account</SignUpTitle>
-          <span>Sign up with your email and password</span>
-          <form onSubmit={this.handleSubmit}>
-            <FormInput
-                autoComplete='nickname'
-                type='text'
-                name='displayName'
-                value={displayName}
-                onChange={this.handleChange}
-                label='Display Name'
-                required
-            />
-            <FormInput
-                autoComplete='email'
-                type='email'
-                name='email'
-                value={email}
-                onChange={this.handleChange}
-                label='Email'
-                required
-            />
-            <FormInput
-                autoComplete='new-password'
-                type='password'
-                name='password'
-                minLength={6}
-                value={password}
-                onChange={this.handleChange}
-                label='Password'
-                required
-            />
-            <FormInput
-                autoComplete='new-password'
-                type='password'
-                minLength={6}
-                name='confirmPassword'
-                value={confirmPassword}
-                onChange={this.handleChange}
-                label='Confirm Password'
-                required
-            />
-            <CustomButton type='submit'>SIGN UP</CustomButton>
-          </form>
-        </SignUpContainer>
+      <SignUpContainer>
+        <SignUpTitle>I do not have a account</SignUpTitle>
+        <span>Sign up with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            autoComplete='nickname'
+            type='text'
+            name='displayName'
+            value={displayName}
+            onChange={this.handleChange}
+            label='Display Name'
+            required
+          />
+          <FormInput
+            autoComplete='email'
+            type='email'
+            name='email'
+            value={email}
+            onChange={this.handleChange}
+            label='Email'
+            required
+          />
+          <FormInput
+            autoComplete='new-password'
+            type='password'
+            name='password'
+            minLength={6}
+            value={password}
+            onChange={this.handleChange}
+            label='Password'
+            required
+          />
+          <FormInput
+            autoComplete='new-password'
+            type='password'
+            minLength={6}
+            name='confirmPassword'
+            value={confirmPassword}
+            onChange={this.handleChange}
+            label='Confirm Password'
+            required
+          />
+          <CustomButton type='submit'>SIGN UP</CustomButton>
+        </form>
+      </SignUpContainer>
     );
   }
 }
